a4b614e081e967b95d61014d8076d8ac3c1cc665
Francois PEGORY <pfrancois_99@yahoo.fr>
2020-05-07 17:12:19 +0200

    ajout yahoo comme source pour europe

---
+++ fp_bc/sources/yahoo.py
@@ -0,0 +1,34 @@
+
+import datetime
+from beancount.prices import source as bean_source
+
+import requests
+from beancount.core.number import D
+
+
+class YahooError(ValueError):
+    "An error from the Yahoo API."
+
+
+class Source(bean_source.Source):
+    def get_latest_price(self, ticker):
+        response = requests.get("https://query1.finance.yahoo.com/v8/finance/chart/%s" % ticker)
+        content = next(iter(response.json(parse_float=D).values()))
+        if response.status_code != requests.codes.ok:
+            raise YahooError("Status {}: {}".format(response.status_code, content['error']))
+        if content['error'] is not None:
+            raise YahooError("Error fetching Yahoo data: {}".format(content['error']))
+        result = content['result'][0]
+        try:
+            price = D(result["meta"]['regularMarketPrice'])
+            timezone = datetime.timezone(
+                datetime.timedelta(hours=result["meta"]['gmtoffset'] / 3600000),
+                result["meta"]['exchangeTimezoneName'])
+            trade_time = datetime.datetime.fromtimestamp(result["meta"]['regularMarketTime'],
+                                                         tz=timezone)
+        except KeyError:
+            raise YahooError("Invalid response from Yahoo: {}".format(repr(result)))
+        currency = result["meta"]["currency"]
+
+        return bean_source.SourcePrice(price, trade_time, currency)
+
