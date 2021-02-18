2b9e61bbac6c5c6de1e36104d1042413933207d1
Francois PEGORY <pfrancois_99@yahoo.fr>
2021-02-18 21:23:04 +0100

    changement suite a independance carte visa

--- fp_bc/importers/sg.py
+++ fp_bc/importers/sg.py
@@ -12,6 +12,7 @@ from beancount.core import account
 from beancount.core import data  # pylint:disable=E0611
 from beancount.core import flags
 from beancount.ingest import importer  # noqa
+from beancount.parser import printer
 
 from fp_bc.utils import CsvUnicodeReader
 from fp_bc import utils
@@ -53,8 +54,8 @@ class ImporterSG(importer.ImporterProtocol):
         account_root: str,
         account_cash: str,
         cat_default: str,
-        cat_frais: str,
         account_visa: str,
+        account_id: str,
     ) -> None:
         self.tiers_update = tiers_update
         self.tiers_cat = tiers_cat
@@ -63,11 +64,11 @@ class ImporterSG(importer.ImporterProtocol):
         self.account_root = account_root
         self.account_cash = account_cash
         self.cat_default = cat_default
-        self.cat_frais = cat_frais
         self.account_visa = account_visa
+        self.account_id = account_id
 
     def identify(self, file: t.IO) -> t.Optional[t.Match[str]]:
-        return re.match(r"Export_\d*_\d*_\d*.csv", path.basename(file.name))
+        return re.match(f"{self.account_id}.csv", path.basename(file.name))
 
     def file_account(self, _: t.IO) -> str:
         return self.account_root
@@ -100,21 +101,17 @@ class ImporterSG(importer.ImporterProtocol):
     def extract(self, file: t.IO, existing_entries: t.Optional[bc_directives] = None) -> t.List[bc_directives]:
         # Open the CSV file and create directives.
         entries = []
-        paiment_carte_visa: t.Dict[str, t.Optional[decimal.Decimal]] = {}
         error = False
         with open(file.name, "r", encoding="windows-1252") as fichier:
             for index, row in enumerate(
-                CsvUnicodeReader(
-                    fichier, champs=["date", "detail", "montant", "devise"], ligne_saut=2, champ_detail="detail",
-                ),
-                4,
+                CsvUnicodeReader(fichier, champs=["date", "libelle", "detail", "montant", "devise"], ligne_saut=2, champ_detail="detail",), 4,
             ):
                 tiers = None
                 cpt2 = None
                 narration = ""
                 meta = data.new_metadata(file.name, index)
                 meta["comment"] = row.detail.strip()
-                meta['update_time'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                meta["update_time"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
                 flag = None
                 try:
                     montant_releve = amount.Amount(utils.to_decimal(row.row["montant"]), self.currency)
@@ -130,9 +127,7 @@ class ImporterSG(importer.ImporterProtocol):
                         error = True
                         continue
                     cpt2 = self.account_cash
-                    posting_1 = data.Posting(
-                        account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None,
-                    )
+                    posting_1 = data.Posting(account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None,)
                     posting_2 = data.Posting(
                         account=cpt2,
                         units=amount.Amount(montant_releve.number * decimal.Decimal("-1"), self.currency),
@@ -168,10 +163,11 @@ class ImporterSG(importer.ImporterProtocol):
                     cpt2 = "Assets:Titre:SG-LDD"
                 if row.in_detail(r"PREL  VERST VOL", row.champ_detail) and row.in_detail(r"DE: SG", row.champ_detail):
                     cpt2 = "Assets:Titre:PEE:Cash"
+                # paiment ou credit carte bleu
+                if "DEBIT MENSUEL CARTE" in row.detail or "CREDIT MENSUEL CARTE" in row.detail:
+                    cpt2 = self.account_visa
                 if cpt2:  # VIREMENT interne
-                    posting_1 = data.Posting(
-                        account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None,
-                    )
+                    posting_1 = data.Posting(account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None,)
                     posting_2 = data.Posting(
                         account=cpt2,
                         units=amount.Amount(montant_releve.number * decimal.Decimal("-1"), self.currency),
@@ -185,6 +181,10 @@ class ImporterSG(importer.ImporterProtocol):
                         narration = "%s => %s" % (short(self.account_root), short(cpt2))
                     else:
                         narration = "%s => %s" % (short(cpt2), short(self.account_root))
+                    if cpt2 == self.account_visa:
+                        links = frozenset((f"{date_releve.strftime('%Y-%m')}",))
+                    else:
+                        links = data.EMPTY_SET
                     transac = data.Transaction(
                         meta=meta,
                         date=date_releve,
@@ -192,7 +192,7 @@ class ImporterSG(importer.ImporterProtocol):
                         payee=tiers.strip(),
                         narration=narration,
                         tags=data.EMPTY_SET,
-                        links=data.EMPTY_SET,
+                        links=links,
                         postings=[posting_1, posting_2],
                     )
                     self.check_before_add(transac)
@@ -200,12 +200,7 @@ class ImporterSG(importer.ImporterProtocol):
                     continue
                 # paiment carte visa
                 if row.in_detail(r"^CARTE \w\d\d\d\d(?! RETRAIT)"):  # cas general de la visa
-                    meta["date_visa"] = str(date_releve)
                     flag = flags.FLAG_OKAY
-                    if date_releve.month == 1:  # si on est en janvier , les depenses sont celles de decembre
-                        annee = date_releve.year - 1
-                    else:
-                        annee = date_releve.year
                     reg_visa = r"(?:CARTE \w\d\d\d\d) (?:REMBT )?(?P<date>\d\d\/\d\d) (?P<desc>.*?)(?:\d+,\d\d|COMMERCE ELECTRONIQUE|$)"
                     retour = re.search(reg_visa, row.detail, re.UNICODE | re.IGNORECASE)
                     if retour:
@@ -215,35 +210,22 @@ class ImporterSG(importer.ImporterProtocol):
                             self.logger.error("attention , probleme regex visa pour operation ligne %s", index)
                             self.logger.error(f"{row.detail}")
                             continue
-                        date = utils.strpdate("%s/%s" % (retour.group("date"), annee), "%d/%m/%Y")
                     else:
                         error = True
                         self.logger.error("attention , probleme regex visa pour operation ligne %s", index)
                         self.logger.error(f"{row.detail}")
                         continue
-                    paiment_carte_visa[meta["date_visa"]] = (
-                        paiment_carte_visa.get(meta["date_visa"], decimal.Decimal("0")) + montant_releve.number
-                    )
-                    posting_1 = data.Posting(
-                        account=self.account_visa, units=montant_releve, cost=None, flag=None, meta=None, price=None,
-                    )
+                    posting_1 = data.Posting(account=self.account_visa, units=montant_releve, cost=None, flag=None, meta=None, price=None,)
                     tiers = self.tiers_update_verifie(tiers).strip()
 
-                    if not tiers:
-                        tiers = "Inconnu"
                     cpt2 = self.cat(tiers)
 
                     posting_2 = data.Posting(
-                        account=cpt2,
-                        units=amount.Amount(montant_releve.number * -1, self.currency),
-                        cost=None,
-                        flag=None,
-                        meta=None,
-                        price=None,
+                        account=cpt2, units=amount.Amount(montant_releve.number * -1, self.currency), cost=None, flag=None, meta=None, price=None,
                     )
                     transac = data.Transaction(
                         meta=meta,
-                        date=date,
+                        date=date_releve,
                         flag=flag,
                         payee=tiers,
                         narration="",
@@ -257,7 +239,7 @@ class ImporterSG(importer.ImporterProtocol):
                 else:
                     if not tiers:
                         #  c'est un virement non transfert
-                        regex_virement = r"VIR EUROPEEN EMIS   LOGITEL POUR: (.*?)(?: \d\d \d\d BQ \d+ CPT \d+)*? REF:"
+                        regex_virement = r"VIR EUROPEEN EMIS \s* LOGITEL POUR: (.*?)(?: \d\d \d\d BQ \d+ CPT \S+)*? REF:"
                         if re.search(regex_virement, row.detail, re.UNICODE | re.IGNORECASE):
                             tiers = re.search(regex_virement, row.detail, re.UNICODE | re.IGNORECASE).group(1)
                         if "VIR EUROPEEN EMIS" in row.detail and not tiers:
@@ -266,7 +248,7 @@ class ImporterSG(importer.ImporterProtocol):
                             continue
                         # prelevement
                         if "PRELEVEMENT EUROPEEN" in row.detail:
-                            tiers = f'{row.in_detail(r" DE: (.+?) ID:")} - {row.in_detail(r"MOTIF: (.+)")}'
+                            tiers = f'{row.in_detail(r" DE: (.+?) ID:")}'
                         else:
                             if "VIR RECU" in row.detail and not tiers:
                                 tiers = row.in_detail(r" DE: (.+?) (?:(?:MOTIF|REF):) ")
@@ -284,16 +266,9 @@ class ImporterSG(importer.ImporterProtocol):
                         cpt2 = "Expenses:Lgmt:Pret"
                     if not cpt2:
                         cpt2 = self.cat_default
-                    posting_1 = data.Posting(
-                        account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None,
-                    )
+                    posting_1 = data.Posting(account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None,)
                     posting_2 = data.Posting(
-                        account=cpt2,
-                        units=amount.Amount(montant_releve.number * -1, self.currency),
-                        cost=None,
-                        flag=None,
-                        meta=None,
-                        price=None,
+                        account=cpt2, units=amount.Amount(montant_releve.number * -1, self.currency), cost=None, flag=None, meta=None, price=None,
                     )
                     flag = flags.FLAG_OKAY
                     transac = data.Transaction(
@@ -308,35 +283,118 @@ class ImporterSG(importer.ImporterProtocol):
                     )
                     self.check_before_add(transac)
                     entries.append(transac)
-        if paiment_carte_visa:
-            i = 0
-            for date_paiment_visa in paiment_carte_visa:
-                i = i + 1
+        if error:
+            raise Exception("au moins une erreur")
+        return entries
+
+
+class ImporterSG_Visa(importer.ImporterProtocol):
+    def __init__(
+        self,
+        tiers_update: t.Sequence[t.Sequence[str]],
+        tiers_cat: t.Sequence[t.Sequence[str]],
+        currency: str,
+        cat_default: str,
+        account_id: str,
+        account_root: str,
+    ) -> None:
+        self.tiers_update = tiers_update
+        self.tiers_cat = tiers_cat
+        self.logger = logging.getLogger(__file__)  # pylint: disable=W0612
+        self.currency = currency
+        self.account_root = account_root
+        self.cat_default = cat_default
+        self.account_id = account_id
+
+    def identify(self, file: t.IO) -> t.Optional[t.Match[str]]:
+        return re.match(f"{self.account_id}.csv", path.basename(file.name))
+
+    def file_account(self, _: t.IO) -> str:
+        return self.account_root
+
+    def extract(self, file: t.IO, existing_entries: t.Optional[bc_directives] = None) -> t.List[bc_directives]:
+        # Open the CSV file and create directives.
+        entries = []
+        error = False
+        with open(file.name, "r", encoding="windows-1252") as fichier:
+            for index, row in enumerate(
+                CsvUnicodeReader(fichier, champs=["date", "date_visa", "detail", "montant", "devise"], ligne_saut=2, champ_detail="detail",), 4,
+            ):
                 tiers = None
                 cpt2 = None
-                meta = data.new_metadata("created on the spot", i)
-                montant = paiment_carte_visa[date_paiment_visa]
-                unit = amount.Amount(utils.to_decimal(montant) * -1, self.currency)
-                unit_opp = amount.Amount(utils.to_decimal(montant), self.currency)
-                posting_1 = data.Posting(
-                    account=self.account_visa, units=unit, cost=None, flag=None, meta=None, price=None,
-                )
+                meta = data.new_metadata(file.name, index)
+                meta["comment"] = row.detail.strip()
+                meta["update_time"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                flag = flags.FLAG_OKAY
+                try:
+                    montant_releve = amount.Amount(utils.to_decimal(row.row["montant"]), self.currency)
+                except decimal.InvalidOperation:
+                    self.logger.error(f"montant '{row.row['montant']}' invalide pour operation ligne {index}")
+                    continue
+                date = utils.strpdate(row.row["date"], "%d/%m/%Y")
+                date_visa = utils.strpdate(row.row["date_visa"], "%d/%m/%Y")
+                reg_visa = r"\d\d\d\d\/(?P<desc>.*) \d"
+                retour = re.search(reg_visa, row.detail, re.UNICODE | re.IGNORECASE)
+                if retour:
+                    tiers = retour.group("desc")
+                    for regle_tiers in self.tiers_update:
+                        try:
+                            if re.search(regle_tiers[0], tiers, re.UNICODE | re.IGNORECASE):
+                                tiers = regle_tiers[1]
+                            else:
+                                tiers = tiers.capitalize()
+                        except TypeError:
+                            error = True
+                            self.logger.error(f"regle:{regle_tiers[0]} - tiers: {tiers}")
+                            self.logger.error("attention , probleme regex visa pour operation ligne %s", index)
+                            self.logger.error(f"{row.detail}")
+                            raise Exception('erreur')
+                    if not tiers:
+                        error = True
+                        self.logger.error("attention , probleme regex visa pour operation ligne %s", index)
+                        self.logger.error(f"{row.detail}")
+                        raise Exception('erreur')
+                else:
+                    error = True
+                    self.logger.error("attention , probleme regex visa pour operation ligne %s", index)
+                    self.logger.error(f"{row.detail}")
+                    raise Exception('erreur')
+
+                cat = None
+                for regle_cat in self.tiers_cat:
+                    if re.search(regle_cat[0], tiers, re.UNICODE | re.IGNORECASE):
+                        cat = regle_cat[1]
+                if not cat:
+                    cat = self.cat_default
+                posting_1 = data.Posting(account=self.account_root, units=montant_releve, cost=None, flag=None, meta=None, price=None)
                 posting_2 = data.Posting(
-                    account=self.account_root, units=unit_opp, cost=None, flag=None, meta=None, price=None,
+                    account=cat, units=amount.Amount(montant_releve.number * -1, self.currency), cost=None, flag=None, meta=None, price=None,
                 )
-                tiers = "Virement"
-                meta["date_visa"] = date_paiment_visa
                 transac = data.Transaction(
                     meta=meta,
-                    date=utils.strpdate(date_paiment_visa),
-                    flag=flags.FLAG_OKAY,
-                    payee=tiers.strip(),
-                    narration=f"{short(self.account_root)} => {short(self.account_visa)}",
+                    date=date,
+                    flag=flag,
+                    payee=tiers,
+                    narration="",
                     tags=data.EMPTY_SET,
-                    links=data.EMPTY_SET,
+                    links=frozenset((f"{date_visa.strftime('%Y-%m')}",)),
                     postings=[posting_1, posting_2],
                 )
-                self.check_before_add(transac)
+                try:
+                    data.sanity_check_types(transac)
+                    for posting in transac.postings:
+                        if posting.account is None:
+                            raise AssertionError(f"error , problem assertion {pprint.pformat(posting)} in the transaction {pprint.pformat(transac)}")
+                    if len(transac.postings) < 2:
+                        raise AssertionError(f"error , problem assertion {pprint.pformat(posting)} in the transaction {pprint.pformat(transac)}")
+                except AssertionError as exc:
+                    try:
+                        print(exc)
+                        printer.print_entry(transac)
+                    except TypeError:
+                        pprint.pprint(transac)
+                        # error = True
+                        break
                 entries.append(transac)
         if error:
             raise Exception("au moins une erreur")
