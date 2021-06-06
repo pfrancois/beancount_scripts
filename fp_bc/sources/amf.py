f7a15b1a6dd867d5e202bb460d78cfccf1fc0c49
Francois PEGORY <pfrancois_99@yahoo.fr>
2021-06-06 16:46:20 +0200

    ajout test afin d'eviter erreur

--- fp_bc/sources/amf.py
+++ fp_bc/sources/amf.py
@@ -14,6 +14,7 @@ from fp_bc import utils
 
 class AmfException(utils.UtilsException):
     """juste une exception qui gere le format"""
+
     pass
 
 
@@ -37,19 +38,18 @@ class Source(bean_source.Source):
         }
         req = requests.get("https://geco.amf-france.org/Bio/rech_part.aspx", params=payload)
         soup = bs4.BeautifulSoup(req.content, "html.parser")
-        values = soup.find_all(class_="ResultatCritereValue")
-        keys = soup.find_all(class_="ResultatCritere")
-        result = dict(zip([v.get_text() for v in keys], [v.get_text() for v in values]))
-        try:
-            date_req = utils.strpdate(result["Date VL :"], fmt="%d/%m/%Y")
-            dt = datetime.datetime(date_req.year, date_req.month, date_req.day, tzinfo=pytz.utc)
-            if (
-                result["Valeur (€) :"][-2:] == "00"
-                and result["Valeur (€) :"].find(",") != -1
-                and len(result["Valeur (€) :"]) > 4
-            ):
-                result["Valeur (€) :"] = result["Valeur (€) :"][:-2]
-            montant_req = utils.to_decimal(s=result["Valeur (€) :"], virgule=True, space=True)
-        except Exception:
-            raise AmfException("%s inconnu pour l'amf", ticker)
-        return bean_source.SourcePrice(montant_req, dt, "EUR")
+        if soup.find("label", {"id": "Nbrep"}) and soup.find("label", {"id": "Nbrep"}).find("caption").string == "Votre recherche a abouti à 0 réponse(s).":
+            log.error("pas de correspondance pour le ticker %s dans la base de l'AMF" % ticker)
+        else:
+            values = soup.find_all(class_="ResultatCritereValue")
+            keys = soup.find_all(class_="ResultatCritere")
+            result = dict(zip([v.get_text() for v in keys], [v.get_text() for v in values]))
+            try:
+                date_req = utils.strpdate(result["Date VL :"], fmt="%d/%m/%Y")
+                dt = datetime.datetime(date_req.year, date_req.month, date_req.day, tzinfo=pytz.utc)
+                if result["Valeur (€) :"][-2:] == "00" and result["Valeur (€) :"].find(",") != -1 and len(result["Valeur (€) :"]) > 4:
+                    result["Valeur (€) :"] = result["Valeur (€) :"][:-2]
+                montant_req = utils.to_decimal(s=result["Valeur (€) :"], virgule=True, space=True)
+                return bean_source.SourcePrice(montant_req, dt, "EUR")
+            except Exception:
+                log.error("erreur pour le ticker %s" % ticker)
