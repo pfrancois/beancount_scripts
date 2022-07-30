db2c22bec6d75cf3fb588b9187bd9a651292c250
Francois PEGORY <pfrancois_99@yahoo.fr>
2022-07-31 00:50:24 +0200

    ajouts type, format , ajout get historical data amf , arround a 0.01

--- fp_bc/sources/amf.py
+++ fp_bc/sources/amf.py
@@ -5,8 +5,12 @@ import datetime
 import time
 import logging
 import pytz
+from dateutil.parser import parse as parse_datetime
 from beancount.prices import source as bean_source
-import bs4  # type: ignore
+from beancount.core.number import D
+import bs4
+import typing
+import datetime
 
 import requests
 from fp_bc import utils
@@ -19,37 +23,108 @@ class AmfException(utils.UtilsException):
 
 
 class Source(bean_source.Source):
-    def get_latest_price(self, ticker):
+    def get_latest_price(self, ticker: str) -> typing.Optional[bean_source.SourcePrice]:
+        try:
+            log = logging.getLogger()
+            log.info(f"AMF:{ticker}")
+            time.sleep(5)
+            payload = {
+                "varvalidform": "on",
+                "NomProd": "",
+                "FAMILLEPROD": "0",
+                "selectNRJ": "0",
+                "NumAgr": "",
+                "CLASSPROD": "0",
+                "CodeISIN": ticker,
+                "NomSOc": "",
+                "action": "new",
+                "valid_form": "Lancer+la+recherche",
+                "sltix": "1+2+3+INVESTMENT+MANAGERS",
+            }
+            req = requests.get("https://geco.amf-france.org/Bio/rech_part.aspx", params=payload)
+            soup = bs4.BeautifulSoup(req.content, "html.parser")
+            if (
+                soup.find("label", {"id": "Nbrep"})
+                and soup.find("label", {"id": "Nbrep"}).find("caption").string == "Votre recherche a abouti à 0 réponse(s)."
+            ):
+                raise AmfException("pas de correspondance pour le ticker %s dans la base de l'AMF" % ticker)
+            else:
+                values = soup.find_all(class_="ResultatCritereValue")
+                keys = soup.find_all(class_="ResultatCritere")
+                result = dict(zip([v.get_text() for v in keys], [v.get_text() for v in values]))
+                try:
+                    date_req = utils.strpdate(result["Date VL :"], fmt="%d/%m/%Y")
+                    timezone = pytz.timezone('Europe/Paris')
+                    dt = timezone.localize(datetime.datetime(date_req.year, date_req.month, date_req.day, 17, 30))
+
+                    thePrice = soup.find("td", text="Valeur (€) :").next_sibling.get_text(strip=True)
+                    montant_req = D(thePrice.replace(" ", "").replace(",", ".")).quantize(D("0.01"))
+                    return bean_source.SourcePrice(montant_req, dt, "EUR")
+                except Exception:
+                    raise AmfException("erreur pour le ticker %s" % ticker)
+        except AmfException as e:
+            log.error(str(e))
+            return None
+
+    def get_historical_price(self, ticker: str, time: datetime.datetime) -> typing.Optional[bean_source.SourcePrice]:
+        """Return the historical price found for the symbol at the given date.
+        This could be the price of the close of the day, for instance. We assume
+        that there is some single price representative of the day.
+        Args:
+          ticker: A string, the ticker to be fetched by the source. This ticker
+            may include structure, such as the exchange code. Also note that
+            this ticker is source-specified, and is not necessarily the same
+            value as the commodity symbol used in the Beancount file.
+          time: The timestamp at which to query for the price. This is a
+            timezone-aware timestamp you can convert to any timezone. For past
+            dates we query for a time that is equivalent to 4pm in the user's
+            timezone.
+        Returns:
+          A SourcePrice instance. If the price could not be fetched, None is
+          returned and another source should be consulted. There is never any
+          guarantee that a price source will be able to fetch its value; client
+          code must be able to handle this. Also note that the price's returned
+          time must be timezone-aware.
+        """
         log = logging.getLogger()
         log.info(f"AMF:{ticker}")
-        time.sleep(5)
-        payload = {
-            "varvalidform": "on",
-            "NomProd": "",
-            "FAMILLEPROD": "0",
-            "selectNRJ": "0",
-            "NumAgr": "",
-            "CLASSPROD": "0",
-            "CodeISIN": ticker,
-            "NomSOc": "",
-            "action": "new",
-            "valid_form": "Lancer+la+recherche",
-            "sltix": "1+2+3+INVESTMENT+MANAGERS",
-        }
-        req = requests.get("https://geco.amf-france.org/Bio/rech_part.aspx", params=payload)
-        soup = bs4.BeautifulSoup(req.content, "html.parser")
-        if soup.find("label", {"id": "Nbrep"}) and soup.find("label", {"id": "Nbrep"}).find("caption").string == "Votre recherche a abouti à 0 réponse(s).":
-            log.error("pas de correspondance pour le ticker %s dans la base de l'AMF" % ticker)
-        else:
-            values = soup.find_all(class_="ResultatCritereValue")
-            keys = soup.find_all(class_="ResultatCritere")
-            result = dict(zip([v.get_text() for v in keys], [v.get_text() for v in values]))
-            try:
-                date_req = utils.strpdate(result["Date VL :"], fmt="%d/%m/%Y")
-                dt = datetime.datetime(date_req.year, date_req.month, date_req.day, tzinfo=pytz.utc)
-                if result["Valeur (€) :"][-2:] == "00" and result["Valeur (€) :"].find(",") != -1 and len(result["Valeur (€) :"]) > 4:
-                    result["Valeur (€) :"] = result["Valeur (€) :"][:-2]
-                montant_req = utils.to_decimal(s=result["Valeur (€) :"], virgule=True, space=True)
-                return bean_source.SourcePrice(montant_req, dt, "EUR")
-            except Exception:
-                log.error("erreur pour le ticker %s" % ticker)
+        s = requests.Session()
+        url = (
+            "https://geco.amf-france.org"
+            + "/Bio/rech_part.aspx?varvalidform=on&CodeISIN="
+            + ticker
+            + "&CLASSPROD=0&NumAgr=&selectNRJ=0&NomProd=&NomSOc=&action=new&valid_form=Lancer+la+recherche"
+        )
+
+        r = s.get(url)
+        soup = bs4.BeautifulSoup(r.text, "html.parser")
+        try:
+            numProd = soup.find("input", {"name": "NumProd"})["value"]
+            numPart = soup.find("input", {"name": "NumPart"})["value"]
+        except Exception:
+            log.error("ISIN introuvable sur AMFGeco")
+            return None
+
+        url = (
+            "https://geco.amf-france.org"
+            + "Bio/info_part.aspx?SEC=VL&NumProd=" + numProd
+            + "&NumPart=" + numPart
+            + "&DateDeb=" + str(time.date().day) + "%2F" + str(time.date().month) + "%2F" + str(time.date().year)
+            + "&DateFin=" + str(time.date().day) + "%2F" + str(time.date().month) + "%2F" + str(time.date().year)
+            + "&btnvalid=OK"
+        )
+
+        r = s.get(url)
+        soup = bs4.BeautifulSoup(r.text, "html.parser")
+        try:
+            theDate = soup.find("tr", class_="ligne2").find_all("td")[0].get_text(strip=True)
+            theDate = parse_datetime(theDate, dayfirst=True)
+            fr_timezone = pytz.timezone("Europe/Paris")
+            theDate = theDate.astimezone(fr_timezone)
+
+            thePrice = soup.find("tr", class_="ligne2").find_all("td")[1].get_text(strip=True)
+            thePrice = D(thePrice.replace(" ", "").replace(",", ".")).quantize(D("0.01"))
+            return bean_source.SourcePrice(thePrice, theDate, "EUR")
+        except Exception:
+            log.error("Pas de valeur liquidative publiée à cette date sur AMFGeco")
+            return None
--- fp_bc/sources/yahoo.py
+++ fp_bc/sources/yahoo.py
@@ -1,10 +1,10 @@
-
 import datetime
 from beancount.prices import source as bean_source
 
 import requests
 from beancount.core.number import D
 import logging
+import pytz
 
 
 class YahooError(ValueError):
@@ -13,28 +13,30 @@ class YahooError(ValueError):
 
 class Source(bean_source.Source):
     def get_latest_price(self, ticker: str) -> bean_source.SourcePrice:
-        log = logging.getLogger()
-        log.info(f"yahoo:{ticker}")
-        response = requests.get(f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}", headers={'User-Agent': None})
         try:
-            content = next(iter(response.json(parse_float=D).values()))
-        except Exception as exc:
-            log.debug(exc)
-            raise YahooError(f"Invalid response from Yahoo:  {response}")
-        if response.status_code != requests.codes.ok:
-            raise YahooError(f"Status {response.status_code}: {content['error']}")
-        if content['error'] is not None:
-            raise YahooError(f"Error fetching Yahoo data: {content['error']}")
-        result = content['result'][0]
-        try:
-            price = D(result["meta"]['regularMarketPrice'])
-            timezone = datetime.timezone(
-                datetime.timedelta(hours=result["meta"]['gmtoffset'] / 3600000),
-                result["meta"]['exchangeTimezoneName'])
-            trade_time = datetime.datetime.fromtimestamp(result["meta"]['regularMarketTime'],
-                                                         tz=timezone)
-        except KeyError:
-            raise YahooError(f"Invalid response from Yahoo: {repr(result)}")
-        currency = result["meta"]["currency"]
-        log.debug(f"price: {price} \t day:{trade_time} \t cur:{currency}")
-        return bean_source.SourcePrice(price, trade_time, currency)
+            log = logging.getLogger()
+            log.info(f"yahoo:{ticker}")
+            response = requests.get(f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}", headers={'User-Agent': None})
+            try:
+                content = next(iter(response.json(parse_float=D).values()))
+            except Exception as exc:
+                log.debug(exc)
+                raise YahooError(f"Invalid response from Yahoo:  {response}")
+            if response.status_code != requests.codes.ok:
+                raise YahooError(f"Status {response.status_code}: {content['error']}")
+            if content['error'] is not None:
+                raise YahooError(f"Error fetching Yahoo data: {content['error']}")
+            result = content['result'][0]
+            try:
+                price = D(result["meta"]['regularMarketPrice']).quantize(D("0.01"))
+                timezone = pytz.timezone(result["meta"]['exchangeTimezoneName'])
+                trade_time = timezone.localize(datetime.datetime.fromtimestamp(result["meta"]['currentTradingPeriod']['regular']['end']))
+            except KeyError:
+                raise YahooError(f"Invalid response from Yahoo: {repr(result)}")
+            currency = result["meta"]["currency"]
+            log.debug(f"yahoo:{ticker} \t price: {price} \t day:{trade_time} \t cur:{currency}")
+            # log.info(f"yahoo:{ticker} \t price: {price} \t day:{trade_time} \t cur:{currency}")
+            return bean_source.SourcePrice(price, trade_time, currency)
+        except YahooError as e:
+            log.error(str(e))
+            return None
